d507176c83064661370e39967bf74471035b9d21
maninserg <manin.serg@gmail.com>
2021-04-16 12:45:48 +0300

    Fix paginator in view

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 3)
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -13,8 +13,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page,
-                                                   'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
