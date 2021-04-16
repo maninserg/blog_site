ad707bf583276b34b65a698e28f752d6b935aa0b
maninserg <manin.serg@gmail.com>
2021-04-16 16:45:18 +0300

    Add tags for posts and filter posts by tags

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -20,12 +21,13 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
-    photo = models.ImageField(upload_to ='photos/%Y/%m/%d/', null=True)
+    photo = models.ImageField(upload_to ='photos/%Y/%m/%d/', null=True, blank=True)
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,
                               default='draft')
+    tags = TaggableManager(blank=True)
 
     class Meta:
         ordering = ('-publish',)
