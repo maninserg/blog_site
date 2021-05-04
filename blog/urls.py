1511c67c845b7d3a0a8d2bee87505cd64a69b269
maninserg <manin.serg@gmail.com>
2021-05-04 12:00:03 +0300

    Add caching by Redis

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 from .feeds import LatestPostsFeed
+from django.views.decorators.cache import cache_page
 
 
 app_name = 'blog'
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', cache_page(60 * 5)(views.post_list), name='post_list'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
